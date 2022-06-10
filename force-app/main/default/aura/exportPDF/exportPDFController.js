d86ae4fdf962dc726cb971bad2ae4e30abf0017d
anupam <anupamsinghal.cs@gmail.com>
2022-06-10 18:53:32 +0530

    Here comes the first bit of Code

---
+++ force-app/main/default/aura/exportPDF/exportPDFController.js
@@ -0,0 +1,6 @@
+({
+    loadRecords : function(component, event, helper) {
+        helper.loadAccounts(component,event,helper);
+        helper.loadContacts(component,event,helper);
+    }
+})
---
+++ force-app/main/default/aura/exportPDF/exportPDFHelper.js
@@ -0,0 +1,36 @@
+({
+    loadAccounts : function(component,event,helper) {
+        var columnsList = [
+            { label: 'Id', fieldName: 'Id', type: 'text' },
+            { label: 'Name', fieldName: 'Name', type: 'text' },
+            { label: 'AccountNumber', fieldName: 'AccountNumber', type: 'text' },
+            { label: 'TickerSymbol', fieldName: 'TickerSymbol', type: 'text' }
+        ];
+        var action = component.get('c.returnAccounts');
+        action.setCallback(this, function (response) {
+            component.set("v.accountColumns", columnsList);
+            if (response.getReturnValue() != null) {
+                var records = response.getReturnValue();
+                component.set("v.accounts", records);
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    loadContacts : function(component,event,helper) {
+        var columnsList = [
+            { label: 'Id', fieldName: 'Id', type: 'text' },
+            { label: 'Name', fieldName: 'Name', type: 'text' },
+            { label: 'Email', fieldName: 'Email', type: 'text' },
+            { label: 'Phone', fieldName: 'Phone', type: 'text' }
+        ];
+        var action = component.get('c.returnAccounts');
+        action.setCallback(this, function (response) {
+            component.set("v.contactColumns", columnsList);
+            if (response.getReturnValue() != null) {
+                var records = response.getReturnValue();
+                component.set("v.contacts", records);
+            }
+        });
+        $A.enqueueAction(action);
+    }
+})
